16c32c6595bd542ff5f28d3630835d3cc9b3563c
Mike Mols <mm@firtal.com>
2019-12-02 21:44:37 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  title: 'Blog by Mike Mols',
+  subtitle: 'Software developer blogging about programming and lif and life',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  menu: [
+    {
+      label: '',
+      path: ''
+    }
+  ],
+
+  author: {
+    name: 'Mike Mols',
+    photo: '/mikemols.jpg',
+    bio: 'Software developer blogging about programming and life',
+    contacts: {
+      email: 'hello@mikemols.dk',
+      twitter: 'mike_mols',
+      github: 'mmmols',
+    }
+  }
+};
---
+++ src/hooks/use-site-metadata.js
@@ -0,0 +1,34 @@
+// @flow
+import { useStaticQuery, graphql } from 'gatsby';
+
+const useSiteMetadata = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query SiteMetaData {
+        site {
+          siteMetadata {
+            author {
+              name
+              bio
+              photo
+              contacts {
+                email
+                twitter
+                github
+              }
+            }
+            url
+            title
+            subtitle
+            copyright
+            disqusShortname
+          }
+        }
+      }
+    `
+  );
+
+  return site.siteMetadata;
+};
+
+export default useSiteMetadata;
